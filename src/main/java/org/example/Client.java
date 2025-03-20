782eeba7194d17d04d509d60a25f9f30f549eb94
Peiman Timaji <peimantimaji@yahoo.com>
2025-03-20 22:41:24 +0000

    Start client functionality

---
+++ src/main/java/org/example/Client.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+    private String username;
+
+    public Client(Socket socket, String username){
+        try {
+            this.socket = socket;
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.username = username;
+        } catch (IOException e){
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
+    }
+
+    //Send messages to ClientHandler
+    public void sendMessage(){
+        try {
+            bufferedWriter.write(username);
+            bufferedReader.newLine();
+            bufferedWriter.flush();
+
+            Scanner scanner = new Scanner(System.in);
+            while (socket.isConnected()){
+                String messageToSend = scanner.nextLine();
+                bufferedWriter.write(username + ": " + messageToSend);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+            }
+        } catch (){
+
+        }
+    }
+}
